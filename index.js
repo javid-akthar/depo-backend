df63bdc7bc59f5786e1af5567c96d79ad84d920b
javid-akthar <aktharjavid74@gmail.com>
2023-02-18 21:44:37 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const env = require('./config/environment');
 const app = express();
-const port = 3009;
+const port = 3010;
 const logger = require('morgan');
 const db = require('./config/mongoose');
 // const handlebars = require('handlebars');
